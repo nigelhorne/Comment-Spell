2633084aef98395a3af2dd252d757124261f1437
Kent Fredric <kentfredric@gmail.com>
2014-09-28 17:37:11 +1300

    Build results of 48e5e62 (on master)

--- lib/Comment/Spell.pm
+++ lib/Comment/Spell.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Comment::Spell;
 
-our $VERSION = '0.001001';
+our $VERSION = '0.001002';
 
 # ABSTRACT: Spell Checking for your comments
 
@@ -155,7 +155,7 @@ Comment::Spell - Spell Checking for your comments
 
 =head1 VERSION
 
-version 0.001001
+version 0.001002
 
 =head1 SYNOPSIS
 
