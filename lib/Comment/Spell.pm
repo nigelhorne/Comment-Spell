a0cb352ca7547b2e2cdad1a991d78738dd3759ec
Kent Fredric <kentfredric@gmail.com>
2017-03-09 04:50:13 +1300

    Build results of be9836d (on master)

--- lib/Comment/Spell.pm
+++ lib/Comment/Spell.pm
@@ -41,6 +41,8 @@ has output_filehandle => (
   },
 );
 
+no Moo;
+
 sub _build_stopwords {
   return Pod::Wordlist->new();
 }
@@ -138,8 +140,6 @@ sub parse_from_file {
 sub parse_from_string {    ## no critic (Subroutines::RequireArgUnpacking)
   return $_[0]->parse_from_document( $_[0]->_ppi_string( $_[1] ) );
 }
-
-no Moo;
 
 1;
 
