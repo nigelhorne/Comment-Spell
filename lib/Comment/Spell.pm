9a1033f171355c09f8ec92788a07c25c89a3457a
Kent Fredric <kentfredric@gmail.com>
2017-03-09 05:10:17 +1300

    Build results of 83821d5 (on master)

--- lib/Comment/Spell.pm
+++ lib/Comment/Spell.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Comment::Spell;
 
-our $VERSION = '0.001002';
+our $VERSION = '0.001003';
 
 # ABSTRACT: Spell Checking for your comments
 
@@ -186,7 +186,7 @@ Comment::Spell - Spell Checking for your comments
 
 =head1 VERSION
 
-version 0.001002
+version 0.001003
 
 =head1 SYNOPSIS
 
